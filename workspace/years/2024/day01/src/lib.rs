b2ac064329251cda8f475e77e189f04f006ace52
Luc Varoqui <luc@varoqui.org>
2024-12-02 01:16:28 +0100

    solve 2024 day 01

--- workspace/years/2024/day01/src/lib.rs
+++ workspace/years/2024/day01/src/lib.rs
@@ -4,9 +4,33 @@ register_solver!(2024, 1, Solver);
 
 pub struct Solver;
 
-impl common::DualDaySolver for Solver {
-    fn solve_1(&self, input: &str) -> DayResult {
-        let _ = input;
-        DayResult::default()
+impl common::MonoDaySolver for Solver {
+    fn solve(&self, input: &str) -> (DayResult, DayResult) {
+        let (mut list_a, mut list_b): (Vec<_>, Vec<_>) = input
+            .lines()
+            .map(|l| {
+                let mut line = l.split_whitespace();
+                (
+                    line.next().unwrap().parse::<i32>().unwrap(),
+                    line.next().unwrap().parse::<i32>().unwrap(),
+                )
+            })
+            .unzip();
+
+        list_a.sort();
+        list_b.sort();
+
+        let res1 = list_a
+            .iter()
+            .zip(list_b.iter())
+            .map(|(a, b)| (a - b).abs())
+            .sum::<i32>();
+
+        let res2 = list_a
+            .iter()
+            .map(|a| a * list_b.iter().filter(|b| **b == *a).count() as i32)
+            .sum::<i32>();
+
+        (DayResult::new(res1), DayResult::new(res2))
     }
 }
